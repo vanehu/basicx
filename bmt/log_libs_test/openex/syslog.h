c0bc97af7545b58e1b60eb19b45ce9c0c95754e1
xurendong <xrdlyj@163.com>
2018-03-02 11:21:07 +0800

    update license terms

--- bmt/log_libs_test/openex/syslog.h
+++ bmt/log_libs_test/openex/syslog.h
@@ -6,37 +6,17 @@
  * E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
  * See the contributors file for names of other contributors.
  *
- * Commercial use of this code in source and binary forms is governed by
- * a LGPL v3 license. You may get a copy from project's root directory
- * and the official website page "www.gnu.org/licenses/lgpl.html".
- * Or else you should get a specific written permission.
+ * Commercial use of this code in source and binary forms is
+ * governed by a LGPL v3 license. You may get a copy from the
+ * root directory. Or else you should get a specific written 
+ * permission from the project author.
  *
- * Individual and educational use of this code in source and binary forms
- * is governed by the following 3-clause BSD license.
+ * Individual and educational use of this code in source and
+ * binary forms is governed by a 3-clause BSD license. You may
+ * get a copy from the root directory. Certainly welcome you
+ * to contribute code of all sorts.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of Redis nor the names of its contributors may be used
- *     to endorse or promote products derived from this software without
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Be sure to retain the above copyright notice and conditions.
  */
 
 #ifndef SYSLOG_SYSLOG_H
